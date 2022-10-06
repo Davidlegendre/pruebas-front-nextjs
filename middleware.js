e09bdf094a54b41a15bdd3fde72a4ac485be64f0
David Legendre <legendre2013@hotmail.com>
2022-10-05 21:57:53 -0500

    commit

--- middleware.js
+++ middleware.js
@@ -2,8 +2,7 @@ import { NextResponse } from "next/server";
 
 export async function middleware(req){
     let verify = req.cookies.get('Token')
-
-    if(!verify)
+    if(verify === undefined)
     {
         return NextResponse.redirect(new URL('/', req.url))
     }
